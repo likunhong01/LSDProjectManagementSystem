2e054b7f9c0e48c8f96f400fb5af7d93ed458860
likunkun <9865029@qq.com>
2019-08-31 17:01:34 +0800

    init
    init

---
+++ LSDProjectManagementSystem/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for LSDProjectManagementSystem project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LSDProjectManagementSystem.settings')
+
+application = get_wsgi_application()
